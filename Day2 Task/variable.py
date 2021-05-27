22c09e5b427723f8873f2de4b6d6a7cb769366c8
patel-jay02 <84786686+patel-jay02@users.noreply.github.com>
2021-05-27 11:25:00 +0530

    Add files via upload

---
+++ Day2 Task/variable.py
@@ -0,0 +1,26 @@
+a=50
+b=50.5
+c="PatelJAy"
+
+print(a)
+print("value of b is :", b)
+print("My name is:", c)
+
+c="JAy"
+print("My name is:", c)
+
+#multiple values to multiple variable
+
+a, b, c= 50, 50.5, "PatelJay"
+
+print(a)
+print("value of b is :", b)
+print("My name is:", c)
+
+#same values to multiple variable
+
+a = b = c = 50
+
+print("value of a is :", a)
+print("value of b is :", b)
+print("value of c is :", c)
